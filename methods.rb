f7159a1406d10ea34f615df5c88ea8a0053e1a3f
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-31 23:51:29 -0400

    commit tree 87a4c946c28684e416199c306fc7543bcccf5d3c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,58 @@
 # Your code here!
+
+# function myfunction(param) {
+#     console.log("Running myfunction");
+#     return param + 1;
+# }
+
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1    # last line of method is return value (implicit)
+# end
+
+
+# function greetProgrammer() {
+#     console.log("Hello, programmer!");
+#   }
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# function greet(name) {
+#     console.log(`Hello, ${name}!`);
+# }
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# function greetWithDefault(name = "programmer") {
+#     console.log(`Hello, ${name}!`);
+# }
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# function add(num1, num2) {
+#     return num1 + num2;
+# }
+
+def add(num1, num2)
+    num1 + num2
+end
+
+# function halve(number) {
+#     if (typeof number !== "number") return null;
+  
+#     return number / 2;
+# }
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    num / 2
+end
